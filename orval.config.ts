93a66f1a1baee533fdc29fc225d665788d92b7b0
EddyPBR <edvaldojunior1310@gmail.com>
2025-02-19 20:36:24 -0300

    🔧 Adjusting configs

--- orval.config.ts
+++ orval.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "orval";
 
 export default defineConfig({
   api: {
-    input: `${process.env.API_URL}/docs/json`,
+    input: `${process.env.NEXT_PUBLIC_API_URL}/docs/json`,
     output: {
       target: "./src/http/api.ts",
       client: "fetch",
       httpClient: "fetch",
       clean: true,
-      baseUrl: "${process.env.API_URL}",
+      baseUrl: "${process.env.NEXT_PUBLIC_API_URL}",
       override: {
         fetch: {
           includeHttpResponseReturnType: false,
